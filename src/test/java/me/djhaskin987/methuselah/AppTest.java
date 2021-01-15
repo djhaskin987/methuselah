83a44bedf715c451193648d91259296935daf582
Daniel Jay Haskin <djhaskin987@gmail.com>
2021-01-14 17:36:23 -0700

    So it begins

---
+++ src/test/java/me/djhaskin987/methuselah/AppTest.java
@@ -0,0 +1,20 @@
+package me.djhaskin987.methuselah;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
